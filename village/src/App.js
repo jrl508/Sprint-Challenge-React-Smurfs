f5522136744a3d5f75bb22bf851f3ae9093df44f
jrl508 <jrl47@yahoo.com>
2019-06-14 13:15:57 -0400

    Routes established, MVP completed

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import {Route} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 class App extends Component {
   constructor(props) {
@@ -43,8 +45,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addSmurf={this.addSmurf}/>
-        <Smurfs smurfs={this.state.smurfs} />
+        <NavLink exact to='/' className="nav-item">Home /</NavLink>
+        <NavLink exact to ='/smurf-form' className="nav-item"> Form</NavLink>
+        <Route exact path='/smurf-form' render ={props => <SmurfForm {...props} addSmurf={this.addSmurf}/>}/>
+        <Route exact path='/' render = {props => <Smurfs {...props} smurfs={this.state.smurfs}/>} />
       </div>
     );
   }
